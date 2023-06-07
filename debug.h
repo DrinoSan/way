b9cb1eff1397b857b59ffdc38e1a7d48c427440a
DrinoSan <s.becirbegovic@gmail.com>
2023-06-08 01:42:06 +0200

    Dissasmbler to show opcode to transform machine code into textual listing of the instruction

---
+++ debug.h
@@ -0,0 +1,12 @@
+#ifndef way_debug_h
+#define way_debug_h
+
+#include "chunk.h"
+
+//-----------------------------------------------------------------------------
+void disassembleChunk(Chunk* chunk, const char* name);
+
+//-----------------------------------------------------------------------------
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif
