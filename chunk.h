ad365a024e3f758fdbd5cbec139189cd25b02e26
DrinoSan <s.becirbegovic@gmail.com>
2023-06-08 00:31:29 +0200

    - Added basic C includes - Created enum for opcode with first command

---
+++ chunk.h
@@ -0,0 +1,11 @@
+#ifndef way_chunk_h
+#define way_chunk_h
+
+#include "common.h"
+
+typedef enum 
+{
+    OP_RETURN,
+} OpCode;
+
+#endif
---
+++ common.h
@@ -0,0 +1,8 @@
+#ifndef way_common_h
+#define way_common_h
+
+#include <stdbool.h> // bool stuff
+#include <stddef.h>  // NULL, size_t stuff
+#include <stdint.h>  // sized integers aka uint8_t stuff
+
+#endif
