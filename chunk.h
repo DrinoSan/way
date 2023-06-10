7b739d272c44fa485917b1914ac715bf9dbe4c42
DrinoSan <s.becirbegovic@gmail.com>
2023-06-10 03:47:00 +0200

    Comments

--- chunk.h
+++ chunk.h
@@ -20,10 +20,26 @@ typedef struct
     ValueArray constants;
 } Chunk;
 
+//-----------------------------------------------------------------------------
+// Initializes Empty chunk
 void initChunk(Chunk* chunk);
+
+//-----------------------------------------------------------------------------
+// Frees used memory of chunk, lines and valueArray
 void freeChunk(Chunk* chunk);
+
+//-----------------------------------------------------------------------------
+// Function to write OP_CODE/Value (Byte) to Chunk
 void writeChunk(Chunk* chunk, uint8_t byte, int line);
+
+//-----------------------------------------------------------------------------
+// Function to handle index larger than 255
+// Uses OP_CONSTANT for indexes lower than 256
+// Uses OP_CONST_LONG for indexes larger than 255
 void writeConstant(Chunk* chunk, Value value, int line);
+
+//-----------------------------------------------------------------------------
+// Function to add Value to chunk->constants pool
 int addConstant(Chunk* chunk, Value value);
 
 #endif
