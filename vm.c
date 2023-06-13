4521f54a4e0995058a4a8a685501d4313825ec19
DrinoSan <s.becirbegovic@gmail.com>
2023-06-13 23:11:40 +0200

    Added new decoding function for instruction OP_CONSTANT

--- vm.c
+++ vm.c
@@ -1,3 +1,7 @@
+//-----------------------------------------------------------------------------
+#include <stdio.h>
+//-----------------------------------------------------------------------------
+#include "Value.h"
 #include "common.h"
 #include "vm.h"
 
@@ -19,11 +23,20 @@ void freeVM()
 static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+
     for(;;)
     {
         uint8_t instruction;
         switch(instruction = READ_BYTE())
         {
+            case OP_CONSTANT: 
+            {
+                Value constant = READ_CONSTANT();
+                printValue(constant);
+                printf("\n");
+                break;
+            }
             case OP_RETURN:
             {
                 return INTERPRET_OK;
@@ -32,6 +45,7 @@ static InterpretResult run()
     }
 
 #undef READ_BYTE
+#undef READ_CONSTANT
 }
 
 //-----------------------------------------------------------------------------
