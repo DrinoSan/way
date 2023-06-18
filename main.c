5f964b5d40979dbb78fde60765da2f57890cdd9a
DrinoSan <s.becirbegovic@gmail.com>
2023-06-18 15:01:21 +0200

    Working arithmetic

--- main.c
+++ main.c
@@ -10,17 +10,28 @@ int main(int argc, const char* argv[])
     Chunk chunk;
     initChunk(&chunk);
 
-
-    writeConstant(&chunk, 200, 124); // Second byte is the index of the constant
-
-    writeConstant(&chunk, 11.2, 125);
+    /* writeConstant(&chunk, 1.2, 125); */
     //int constant2 = addConstant(&chunk, 11.2); // We write the value 1.2 manually in the chunks constant pool. The return value is the index of the added value
     //writeChunk(&chunk, OP_CONSTANT, 125); // The first byte is the operand
     //writeChunk(&chunk, constant2, 125); // Second byte is the index of the constant
 
-    writeChunk(&chunk, OP_NEGATE,  125);
+    // Arithmetic test
+    // 1.2 + 3.4
+    writeConstant(&chunk, 1.2, 123);
+    writeConstant(&chunk, 3.4, 123);
+    writeChunk(&chunk, OP_ADD, 123);
+
+
+    // 4.6 (Result from above) / 5.6
+    writeConstant(&chunk, 5.6, 123);
+    writeChunk(&chunk, OP_DIVIDE, 123);
+
+
+    // Arithmetic test end
+
+    writeChunk(&chunk, OP_NEGATE,  123);
                                      
-    writeChunk(&chunk, OP_RETURN, 125);
+    writeChunk(&chunk, OP_RETURN, 123);
 
     // DisassembleChunk transforms machine code into textual listings of the instructions
     /* disassembleChunk(&chunk, "test chunk"); */
