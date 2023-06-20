848b23c5c652e5d1151f9e0863f2fe6e118bf417
DrinoSan <s.becirbegovic@gmail.com>
2023-06-20 22:21:04 +0200

    Started finishing compiler Work in progress

--- compiler.h
+++ compiler.h
@@ -1,8 +1,10 @@
 #ifndef way_compiler_h
 #define way_compiler_h
 
+//-----------------------------------------------------------------------------
+#include "vm.h"
 
 //-----------------------------------------------------------------------------
-void compile(const char* source);
+bool compile(const char* source, Chunk* chunk);
 
 #endif
